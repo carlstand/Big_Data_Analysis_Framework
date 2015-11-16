276a9f89bbfcea6f90967b7e63267d226103810d
Carl Chen <carlstand@gmail.com>
2015-11-16 08:59:10 +0800

    upload the complete plot.py

--- Python_Mail_miner/plot.py
+++ Python_Mail_miner/plot.py
@@ -25,4 +25,33 @@ print len(times)
 print times
 
 freq = collections.OrderedDict(
-	s
+	sorted(collections.Counter(subjects).items(),
+	 key=lambda t:t[1]))
+print freq
+
+
+plt.figure(1)
+plt.figure(2)
+
+bar_width = 0.35
+plt.figure(1)
+numberOfDay = 10
+keys, values = keysAndValues(times)
+index = np.arange(len(keys[:numberOfDay]))
+plt.bar(index, values[:numberOfDay], bar_width, alpha=0.4, color='b',
+	label='Date')
+
+plt.xticks(index+bar_width, keys[:numberOfDay])
+plt.legend()
+
+plt.figure(2)
+numberOfWord = 10
+keys, values = keysAndValues(freq)
+index = np.arange(len(keys[:numberOfWord]))
+plt.bar(index, values[:numberOfWord], bar_width, alpha=0.4, color='r',
+	label='Word Frequency')
+
+plt.xticks(index+bar_width, keys[:numberOfWord])
+plt.legend()
+
+plt.show()
